e640c7c851b8b38719e5bafeb2d4befdc288b933
xy7-ux <ostrowskiszkola8@gmail.com>
2025-03-11 14:17:50 +0100

    Initial Commit

---
+++ BucketList/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Patryk Ostrowski on 06/03/2025.
+//
+
+import MapKit
+import SwiftUI
+
+struct ContentView: View {
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+        )
+     )
+    
+    @State private var viewModel = ViewModel()
+    
+    var body: some View {
+        if viewModel.isUnlocked {
+            MapReader { proxy in
+                Map(initialPosition: startPosition) {
+                    ForEach(viewModel.locations) { location in
+                        Annotation(location.name, coordinate: location.coordinate) {
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundStyle(.red)
+                                .frame(width: 44, height: 44)
+                                .background(.white)
+                                .clipShape(.circle)
+                                .onLongPressGesture(minimumDuration: 0.2) {
+                                    viewModel.selectedPlace = location
+                                    print("Long press detected")
+                                }
+                            
+                        }
+                    }
+                }
+                .mapStyle(.hybrid(elevation: .realistic))
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                        viewModel.addLocation(at: coordinate)
+                    }
+                }
+                .sheet(item: $viewModel.selectedPlace) { place in
+                    EditView(location: place) {
+                        viewModel.update(location: $0)
+                    }
+                }
+            }
+        } else {
+            Button("Unlock places", action: viewModel.authenticate)
+                .padding()
+                .background(.blue)
+                .foregroundStyle(.white)
+                .clipShape(.capsule)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
